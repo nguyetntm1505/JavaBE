e9701de16ea9158ab92c26d1bad2c577fe3c1dff
nguyetntm1505 <nguyenthiminhnguyet1505@gmail.com>
2024-06-25 22:18:55 +0700

    nop bai tap 2

---
+++ thuc_hanh_java/src/ss4_condition/ex2.java
@@ -0,0 +1,48 @@
+package ss4_condition;
+
+import java.util.Scanner;
+
+//Nhập vào 1 số tự nhiên từ 1 đến 10, sau đó in ra tiếng Anh của từ đó
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap vao so tu nhien n: ");
+        int n = scanner.nextInt();
+
+        switch (n) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+            case 10:
+                System.out.println("ten");
+                break;
+            default:
+                System.out.println("So khong hop le");
+
+        }
+    }
+}
